7915767049b60140effeae523daf0d4b3e8b1359
RajeshBenarjee <rajeshbenarjee01@gmail.com>
2026-03-14 13:51:33 +0530

    Reconfiguring deployment

--- utils/text_processing.py
+++ utils/text_processing.py
@@ -1,16 +1,28 @@
 import re
 import nltk
 
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+def ensure_nltk_data():
+    resources = ["punkt", "punkt_tab"]
+    for resource in resources:
+        try:
+            if resource == "punkt":
+                nltk.data.find("tokenizers/punkt")
+            else:
+                nltk.data.find("tokenizers/punkt_tab")
+        except LookupError:
+            nltk.download(resource, quiet=True)
+
+ensure_nltk_data()
 
 def preprocess_text(text):
     text = text.lower()
-    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r"\s+", " ", text)
     return text.strip()
 
 def sentence_tokenize(text):
-    sentences = nltk.sent_tokenize(text)
+    try:
+        sentences = nltk.sent_tokenize(text)
+    except LookupError:
+        ensure_nltk_data()
+        sentences = nltk.sent_tokenize(text)
     return [s.strip() for s in sentences if len(s.strip()) > 5]
